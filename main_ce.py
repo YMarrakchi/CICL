b0beeb49e1b3fe69b8628b56509977245eff77ba
YMarrakchi <43812077+YMarrakchi@users.noreply.github.com>
2021-11-24 12:32:06 +0100

    Update main_ce.py

--- main_ce.py
+++ main_ce.py
@@ -116,18 +116,19 @@ def parse_option():
 def set_loader(opt):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     train_transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomGrayscale(p=0.2),
-        transforms.ToTensor(),
-        normalize,
+      transforms.RandomResizedCrop(224),
+      transforms.RandomHorizontalFlip(),
+      transforms.ColorJitter(
+        brightness=0.4,
+        contrast=0.4,
+        saturation=0.4,
+        hue=0.2),
+      transforms.ToTensor(),
+      normalize,
     ])
 
     val_transform = transforms.Compose([
         transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         normalize,
     ])
